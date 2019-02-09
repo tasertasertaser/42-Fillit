19e807ef309bd5ee838df965c33bf2dd82762e58
Crystal Schuller <cschulle@e1z3r2p2.42.us.org>
2019-02-08 23:33:11 -0800

    parser fix for larger files

--- ProjectFiles/fillitproject/srcs/parser.c
+++ ProjectFiles/fillitproject/srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: cschulle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 11:08:25 by cschulle          #+#    #+#             */
-/*   Updated: 2019/02/08 21:34:07 by cschulle         ###   ########.fr       */
+/*   Updated: 2019/02/08 23:32:00 by cschulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,47 +26,47 @@
 **	- closes file
 **	- returns string containing file contents
 */
-/*
-char	*parser(char *filename)
-{
-	char	*filecontents;
-	char	buf[167];
-	int		fd;
-	int		i[2];
-	int		filesize;
 
-	i[0] = 0;
-	i[1] = -1;
-	fd = open(filename, O_RDONLY);
-	filesize = read(fd, buf, 167);
-	filecontents = malloc((size_t)filesize - (filesize % 16)); 		// be sure to free after use in calling function!
-	while (i[0] < filesize)
+int		charcount(int i, char buf[])
+{
+	int j;
+	int count;
+
+	j = 0;
+	count = 0;
+	while (j < i)
 	{
-		if (buf[i[0]] == '.' || buf[i[0]] == '#')
-			filecontents[++(i[1])] = buf[i[0]];
-		(i[0])++;
+		if (buf[j] == '.' || buf[j] == '#')
+			count++;
+		j++;
 	}
-	filecontents[(i[1]) + 2] = '\0';
-	close(fd);
-	return (filecontents);
+	return (count);
 }
-*/
+
 char	*parser(char *filename)
 {
 	char	*filecontents;
-	char	buf[54];
+	char	buf[544];
 	int		fd;
 	int		i;
+	int		filesize;
 
 	fd = open(filename, O_RDONLY);
-	read(fd, buf, 544);
-	filecontents = ft_strnew(1);
-	i = 0;
-	while (buf[i])
+	i = read(fd, buf, 544);
+	filesize = charcount(i, buf);
+	if (!(filecontents = malloc((size_t)filesize + 1)))		// be sure to free after use in calling function!
+		return (NULL);
+	filecontents[filesize] = '\0';
+	filesize--;
+	while (filesize >= 0)
 	{
 		if (buf[i] == '.' || buf[i] == '#')
-			filecontents = ft_strjoin(filecontents, &buf[i]);
-		i++;
+		{
+			filecontents[filesize] = buf[i];
+			filesize--;
+		}
+		i--;
 	}
-	return(filecontents);
+	close(fd);
+	return (filecontents);
 }
